8fa7388f0d305179e37b22a83e5eaf4d03dc0c1a
Ericmohn <eric_mohn@hotmail.com>
2022-11-25 17:18:02 -0300

    first commit

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    name: 'SpongeBob Squarepants',
+    age: 50,
+    image:
+      'https://imgix.ranker.com/user_node_img/50095/1001880179/original/a-sick-spongebob-photo-u1?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=375',
+  },
+  {
+    id: 2,
+    name: 'Patrick Star',
+    age: 38,
+    image:
+      'https://i.redd.it/w8cd51e978g51.jpg',
+  },
+  {
+    id: 3,
+    name: 'Squidward Q. Tentacles',
+    age: 44,
+    image:
+      'https://www.nicepng.com/png/detail/179-1795474_handsome-squidward-png-handsome-squidward.png',
+  },
+  {
+    id: 4,
+    name: 'Eugene Mr. Krabs',
+    age: 79,
+    image:
+      'https://imgix.ranker.com/user_node_img/50095/1001880200/original/mr-krabs-could-use-a-break-photo-u1?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=375',
+  },
+  {
+    id: 5,
+    name: 'Sheldon J. Plankton',
+    age: 59,
+    image:
+      'https://i.redd.it/0f3w6cwrcpp51.jpg',
+  },
+];
